b2dfcf82a7f079e3f455bde868ac5b4dc5043f79
gy_pro <690507533_pro@qq.com>
2020-07-14 20:50:52 +0800

    update cloud-consumer-order80 to eureka client

--- cloud-consumer-order80/src/main/java/com/gy/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/gy/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.gy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author gaoyue
  * @create 2020-07-01 20:45
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
